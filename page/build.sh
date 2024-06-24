ab22580f876ed0626c294c09cada91c803ea97a1
CSneko <minecrafthetun@outlook.com>
2024-06-24 23:16:05 +0800

    new

--- page/build.sh
+++ page/build.sh
@@ -6,7 +6,4 @@ npm install hexo
 # 构建hexo
 npx hexo clean
 npx hexo generate
-# 删除无用文件
-find . -mindepth 1 -not -path "./public" -not -path "./public/*" -exec rm -rf {} +
-# 移动文件
-mv ./public/* ./
+
